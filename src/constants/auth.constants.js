bd2d5ba4fef5bbe5fedae3c3d2ead7c41c487841
Pushkar Dangi <pushkardangi746@gmail.com>
2025-08-09 20:22:26 +0530

    refactor: add localhost to url to cors options

--- src/constants/auth.constants.js
+++ src/constants/auth.constants.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: process.env.CORS_ORIGIN || "http://localhost:5173", // Allow only your frontend
+  origin: ["http://localhost:5173", process.env.CORS_ORIGIN],
   credentials: true, // Allow cookies & authentication
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type",
