a3a5f145cb01d1e7ea88635a1b4ab40ad22a6fea
Pushkar Dangi <pushkardangi746@gmail.com>
2025-08-16 16:09:04 +0530

    chore: add new cors origin url

--- src/constants/auth.constants.js
+++ src/constants/auth.constants.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: ["http://localhost:5173", process.env.CORS_ORIGIN],
+  origin: ["http://localhost:5173", process.env.CORS_ORIGIN, process.env.CORS_DEV],
   credentials: true, // Allow cookies & authentication
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type",
